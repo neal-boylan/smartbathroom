cd495a8a8e891866c206acacd045c4b8351608cb
Neal Boylan <120754481+neal-boylan@users.noreply.github.com>
2024-01-05 18:20:31 +0000

    Delete Smart_Bathroom directory

--- Smart_Bathroom/config.h
+++
@@ -1,9 +0,0 @@
-#define WIFI_NAME "VMDFEF5EF"   
-#define WIFI_PASSWORD "2epPHrm6kvrp"
-
-#define DATABASE_URL "smart-bathroom-6bd1b-default-rtdb.europe-west1.firebasedatabase.app" 
-#define DATABASE_SECRET "27DwotqVfXSIRUkP9eIkzbDqjXf1fJbT2bqOWspp"
-
-#define BLYNK_TEMPLATE_ID "TMPL4jtrdvdGa"
-#define BLYNK_TEMPLATE_NAME "Smart Bathroom"
-#define BLYNK_AUTH_TOKEN "M97_huC9qg0X5lXT5KpR4mWGDuQJ83aY"
